9072089e13c6be64ec19fbd48223be5b9ac697ae
nitinrlr <ramaling@sc.edu>
2026-09-12 16:21:54 -0400

    fixed sun emoji, added assign5 to class page

--- csce242/assignments/05jsbutton/script.js
+++ csce242/assignments/05jsbutton/script.js
@@ -1,34 +1,21 @@
 // Speech Bubble
-const speechColumn = document.getElementById("speech");
-const speechBubble = document.getElementById("speech-bubble");
-
-const showSpeech = () => {
-    speechBubble.style.display = "block";
-};
-
-speechColumn.addEventListener("click", showSpeech);
+document.getElementById("speech").onclick = () => {
+    document.getElementById("speech-bubble").style.display = "block";
+}
 
 // Beverage
-const drinkSelect = document.getElementById("drink");
-const drinkMessage = document.getElementById("drink-message");
+document.getElementById("drink").onchange = () => {
+    const drinkSelect = document.getElementById("drink");
+    const selectedDrink = drinkSelect.options[drinkSelect.selectedIndex].text;
 
-const showDrink = () => {
-    const selectedDrink =
-        drinkSelect.options[drinkSelect.selectedIndex].text;
-    drinkMessage.textContent = `${selectedDrink}: Nice Choice!`;
+    document.getElementById("drink-message").innerHTML =
+        selectedDrink + ": Nice Choice!";
 };
 
-drinkSelect.addEventListener("change", showDrink);
-
 // Sun sticker
-const sun = document.getElementById("sun");
-const stickerColumn = document.getElementById("sticker");
-
-const addSticker = () => {
+document.getElementById("sun").onclick = () => {
     const sticker = document.createElement("span");
     sticker.textContent = "☀️";
     sticker.classList.add("emoji-sticker");
-    stickerColumn.appendChild(sticker);
+    document.getElementById("sticker").appendChild(sticker);
 };
-
-sun.addEventListener("click", addSticker);
